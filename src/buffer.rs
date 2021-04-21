37063496b4b61e4d8d2733015751616105b2b7bd
hatoo <hato2000@gmail.com>
2021-04-21 19:19:07 +0900

    Concurrent BufferPoolManager

--- src/buffer.rs
+++ src/buffer.rs
@@ -2,6 +2,7 @@ use crate::disk::Aligned;
 use crate::disk::DiskManager;
 use crate::disk::PageId;
 use async_rwlock::RwLock;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::{
     ops::{Deref, DerefMut},
     sync::Arc,
@@ -37,7 +38,7 @@ pub struct Buffer {
 }
 
 struct Frame<T> {
-    usage_count: u64,
+    usage_count: AtomicU64,
     buffer: Arc<T>,
 }
 
@@ -46,7 +47,7 @@ struct BufferPool<T> {
     next_victim_id: BufferId,
 }
 
-impl<T> BufferPool<T> {
+impl<T: std::fmt::Debug> BufferPool<T> {
     fn new(pool_size: usize) -> Self {
         assert!(pool_size > 0);
         let mut buffers = Vec::with_capacity(pool_size);
@@ -59,7 +60,7 @@ impl<T> BufferPool<T> {
         }
     }
 
-    fn evict(&mut self) -> Option<(BufferId, Option<Arc<T>>)> {
+    fn evict(&mut self) -> Option<(BufferId, Option<T>)> {
         let pool_size = self.size();
         let mut consecutive_pinned = 0;
 
@@ -72,12 +73,14 @@ impl<T> BufferPool<T> {
                     return Some((next_victim_id, None));
                 }
                 Some(f) => {
-                    if f.usage_count == 0 {
-                        let last = frame.take().map(|g| g.buffer);
+                    if f.usage_count.load(Ordering::Acquire) == 0
+                    // Always Arc::strong_count(&f.buffer) == 1
+                    {
+                        let last = frame.take().map(|g| Arc::try_unwrap(g.buffer).unwrap());
                         return Some((next_victim_id, last));
                     }
                     if Arc::get_mut(&mut f.buffer).is_some() {
-                        f.usage_count -= 1;
+                        f.usage_count.fetch_sub(1, Ordering::Release);
                         consecutive_pinned = 0;
                     } else {
                         consecutive_pinned += 1;
@@ -93,7 +96,7 @@ impl<T> BufferPool<T> {
 
     fn insert(&mut self, buffer_id: BufferId, value: T) {
         self.buffers[buffer_id.0] = Some(Frame {
-            usage_count: 0,
+            usage_count: AtomicU64::new(0),
             buffer: Arc::new(value),
         });
     }
@@ -102,17 +105,39 @@ impl<T> BufferPool<T> {
         self.buffers.len()
     }
 
-    fn get(&mut self, buffer_id: BufferId) -> Arc<T> {
-        let frame = self.buffers[buffer_id.0].as_mut().unwrap();
-        frame.usage_count += 1;
+    fn get(&self, buffer_id: BufferId) -> Arc<T> {
+        let frame = self.buffers[buffer_id.0].as_ref().unwrap();
+        frame.usage_count.fetch_add(1, Ordering::Release);
         frame.buffer.clone()
+    }
+}
+
+struct PagePool {
+    pool: BufferPool<Buffer>,
+    page_table: std::collections::HashMap<PageId, BufferId>,
+}
+
+impl PagePool {
+    async fn alloc_page(&mut self, disk: &DiskManager) -> Result<BufferId, Error> {
+        let (buffer_id, prev_buffer) = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
+        if let Some(prev_buffer) = prev_buffer {
+            let lock = prev_buffer.page.read().await;
+            if lock.is_dirty {
+                if let Err(err) = disk.write_page_data(prev_buffer.page_id, &lock).await {
+                    std::mem::drop(lock);
+                    self.pool.insert(buffer_id, prev_buffer);
+                    return Err(err.into());
+                }
+            }
+            self.page_table.remove(&prev_buffer.page_id);
+        }
+        Ok(buffer_id)
     }
 }
 
 pub struct BufferPoolManager {
     disk: DiskManager,
-    pool: BufferPool<Buffer>,
-    page_table: std::collections::HashMap<PageId, BufferId>,
+    page_pool: RwLock<PagePool>,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -127,68 +152,69 @@ impl BufferPoolManager {
     pub fn new(disk: DiskManager, pool_size: usize) -> Self {
         Self {
             disk,
-            pool: BufferPool::new(pool_size),
-            page_table: Default::default(),
+            page_pool: RwLock::new(PagePool {
+                pool: BufferPool::new(pool_size),
+                page_table: Default::default(),
+            }),
         }
     }
 
-    pub async fn fetch_page(&mut self, page_id: PageId) -> Result<Arc<Buffer>, Error> {
-        if let Some(&buffer_id) = self.page_table.get(&page_id) {
-            let buffer = self.pool.get(buffer_id);
-            debug_assert_eq!(buffer.page_id, page_id);
-            Ok(buffer)
-        } else {
-            let buffer_id = self.alloc_buffer().await?;
+    pub async fn fetch_page(&self, page_id: PageId) -> Result<Arc<Buffer>, Error> {
+        {
+            let lock = self.page_pool.read().await;
 
-            let mut page = Aligned::default();
-            self.disk.read_page_data(page_id, &mut page).await?;
-
-            self.page_table.insert(page_id, buffer_id);
-            let buffer = Buffer {
-                page_id: page_id,
-                page: RwLock::new(Page {
-                    is_dirty: false,
-                    page: Box::new(page),
-                }),
-            };
-            self.pool.insert(buffer_id, buffer);
-            Ok(self.pool.get(buffer_id))
-        }
-    }
-
-    pub async fn create_page(&mut self) -> Result<Arc<Buffer>, Error> {
-        let buffer_id = self.alloc_buffer().await?;
-        let page_id = self.disk.allocate_page();
-
-        self.page_table.insert(page_id, buffer_id);
-        let buffer = Buffer {
-            page_id: page_id,
-            page: Default::default(),
-        };
-        self.pool.insert(buffer_id, buffer);
-        Ok(self.pool.get(buffer_id))
-    }
-
-    async fn alloc_buffer(&mut self) -> Result<BufferId, Error> {
-        let (buffer_id, prev_buffer) = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
-        if let Some(prev_buffer) = prev_buffer {
-            let lock = prev_buffer.page.read().await;
-            self.page_table.remove(&prev_buffer.page_id);
-            if lock.is_dirty {
-                self.disk
-                    .write_page_data(prev_buffer.page_id, &lock)
-                    .await?;
+            if let Some(&buffer_id) = lock.page_table.get(&page_id) {
+                let buffer = lock.pool.get(buffer_id);
+                debug_assert_eq!(buffer.page_id, page_id);
+                return Ok(buffer);
             }
         }
-        Ok(buffer_id)
+
+        let mut lock = self.page_pool.write().await;
+
+        if let Some(&buffer_id) = lock.page_table.get(&page_id) {
+            let buffer = lock.pool.get(buffer_id);
+            debug_assert_eq!(buffer.page_id, page_id);
+            return Ok(buffer);
+        }
+
+        let buffer_id = lock.alloc_page(&self.disk).await?;
+        let mut page = Aligned::default();
+        self.disk.read_page_data(page_id, &mut page).await?;
+
+        lock.page_table.insert(page_id, buffer_id);
+        let buffer = Buffer {
+            page_id,
+            page: RwLock::new(Page {
+                is_dirty: false,
+                page: Box::new(page),
+            }),
+        };
+        lock.pool.insert(buffer_id, buffer);
+        Ok(lock.pool.get(buffer_id))
+    }
+
+    pub async fn create_page(&self) -> Result<Arc<Buffer>, Error> {
+        let mut lock = self.page_pool.write().await;
+        let buffer_id = lock.alloc_page(&self.disk).await?;
+        let page_id = self.disk.allocate_page();
+
+        lock.page_table.insert(page_id, buffer_id);
+        let buffer = Buffer {
+            page_id,
+            page: Default::default(),
+        };
+        lock.pool.insert(buffer_id, buffer);
+        Ok(lock.pool.get(buffer_id))
     }
 }
 
 impl Drop for BufferPoolManager {
     fn drop(&mut self) {
         futures::executor::block_on(async {
-            for (page_id, buffer_id) in self.page_table.drain() {
-                let buffer = self.pool.get(buffer_id);
+            let lock = self.page_pool.read().await;
+            for (&page_id, &buffer_id) in lock.page_table.iter() {
+                let buffer = lock.pool.get(buffer_id);
                 let lock = buffer.page.read().await;
                 if lock.is_dirty {
                     self.disk.write_page_data(page_id, &lock).await.unwrap();
@@ -213,14 +239,14 @@ mod tests {
         rng.fill(&mut memory[..]);
         {
             let disk_manager = DiskManager::open(&path).unwrap();
-            let mut buffer_pool_manager = BufferPoolManager::new(disk_manager, 16);
+            let buffer_pool_manager = BufferPoolManager::new(disk_manager, 16);
             let buffer = buffer_pool_manager.create_page().await.unwrap();
             buffer.page.write().await.copy_from_slice(&memory[..]);
         }
 
         {
             let disk_manager = DiskManager::open(&path).unwrap();
-            let mut buffer_pool_manager = BufferPoolManager::new(disk_manager, 16);
+            let buffer_pool_manager = BufferPoolManager::new(disk_manager, 16);
             let buffer = buffer_pool_manager
                 .fetch_page(PageId::first())
                 .await
