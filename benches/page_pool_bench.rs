f7f40fd024a2a1d07d2dfaf7f40e63e5ce3a6fd2
hatoo <hato2000@gmail.com>
2021-04-30 18:22:11 +0900

    Rename bench name

--- benches/page_pool_bench.rs
+++ benches/page_pool_bench.rs
@@ -2,7 +2,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let rt = tokio::runtime::Runtime::new().unwrap();
-    c.bench_function("pool_manager", |b| {
+    c.bench_function("pool_manager_simple", |b| {
         b.to_async(&rt).iter(|| async {
             use haneru::buffer::BufferPoolManager;
             use haneru::disk::DiskManager;
